ea35014166b10583487ca9f6850fe28d03fdc26e
aditya jaiswal <adityajaiswal.laptop@gmail.com>
2025-11-20 10:28:43 +0545

    2nd push

---
+++ week3/MathOperations.java
@@ -0,0 +1,17 @@
+package week3;
+
+import java.util.Scanner;
+
+public class MathOperations
+{
+    public static void main(String[]arg)
+    {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the grade in number=");
+        int grade= sc.nextInt();
+        
+        String result=(grade>=40)?"pass":(grade<40)?"fail":"invalid input";
+        
+        System.out.println(result);
+    }
+}
